d3fd988db7aa411408a11121714553e3ed46c19b
kywsc <fireyw@gmail.com>
2019-12-05 23:41:36 +0900

    Updates

--- precache-manifest.b9ac9efe2dadc495aa0c59aaa6b9f597.js
+++ precache-manifest.b9ac9efe2dadc495aa0c59aaa6b9f597.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "508a005321616e32fa9f51bf4c95d3ed",
+    "revision": "043751096855f84a794f70c7ac83fd54",
     "url": "/movie_app_2019/index.html"
   },
   {
-    "revision": "d6abe6948b0a04d6a9f2",
+    "revision": "0a4e06b548cd3323e9a9",
     "url": "/movie_app_2019/static/css/main.36cc1f04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2019/static/js/2.f2b2c7bc.chunk.js"
   },
   {
-    "revision": "d6abe6948b0a04d6a9f2",
-    "url": "/movie_app_2019/static/js/main.86127512.chunk.js"
+    "revision": "0a4e06b548cd3323e9a9",
+    "url": "/movie_app_2019/static/js/main.ca889fa9.chunk.js"
   },
   {
     "revision": "26c5953caa52b1651c1d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2019/precache-manifest.a8a6e9863e1acaeb29a7268ca6b7a52f.js"
+  "/movie_app_2019/precache-manifest.b9ac9efe2dadc495aa0c59aaa6b9f597.js"
 );
 
 self.addEventListener('message', (event) => {
